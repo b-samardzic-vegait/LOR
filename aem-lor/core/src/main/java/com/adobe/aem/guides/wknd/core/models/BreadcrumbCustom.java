c7adbeb21f87a67c5bb4566d95aa60b7d021232f
Srbislav Vucenovic <s.vucenovic@vegait.rs>
2022-04-05 13:19:54 +0200

    added component

---
+++ aem-lor/core/src/main/java/com/adobe/aem/guides/wknd/core/models/BreadcrumbCustom.java
@@ -0,0 +1,9 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BreadcrumbCustom {
+
+    List<Map<String, String>> getBreadcrumbDetailsWithMap();
+}
