ced8813172fd5ffa323e0fd1673163f5dc173b1f
aleksandarcopic-vegait <aleksandar.copic@vegait.rs>
2022-04-14 12:37:24 +0200

    youtube video supports all types of links

--- aem-lor/core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/CarouselCustomImpl.java
+++ aem-lor/core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/CarouselCustomImpl.java
@@ -2,6 +2,9 @@ package com.adobe.aem.guides.wknd.core.models.impl;
 
 import java.util.List;
 import java.util.Map;
+
+import javax.lang.model.util.ElementScanner6;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -39,9 +42,60 @@ public class CarouselCustomImpl implements CarouselCustom {
             if (carousel != null) {
                 for (Resource slide : carousel.getChildren()) {
                     Map<String, String> slideProperties = new HashMap<>();
+
+                    String assetType = slide.getValueMap().get("assetType", String.class);
+                    String iAssetLink = slide.getValueMap().get("asset", String.class);
+                    String oAssetLink = "https://www.youtube.com/embed/";
+                    int startIndex = 0, endIndex = 0;
+                    boolean startIndexFlag = false, endIndexFlag = false;                               
+
+                    if(assetType.equals("youtubevideo"))
+                    {
+                        if(iAssetLink.toLowerCase().contains("youtu.be")) /*  Short link  */
+                        {                            
+                            startIndex = iAssetLink.indexOf("youtu.be/") + 9;
+                            endIndex = iAssetLink.length();
+
+                            oAssetLink += iAssetLink.substring(startIndex, endIndex);
+
+                            slideProperties.put("asset", oAssetLink);
+                        } 
+                        else /*  Full link/embed link  */
+                        {
+                            for(int i = 0; i < iAssetLink.length(); i++)
+                            {
+                                char c = iAssetLink.charAt(i);
+                                if(c == '=' && startIndexFlag == false){
+                                    startIndex = i + 1;
+                                    startIndexFlag = true;
+                                } else if (c == '&' && endIndexFlag == false){
+                                    endIndex = i;
+                                    endIndexFlag = true;
+                                }                        
+                            }
+    
+                            if(startIndex == 0)
+                            {
+                                oAssetLink = iAssetLink;
+                            } else if (endIndex == 0)
+                            {
+                                endIndex = iAssetLink.length();
+                                oAssetLink += iAssetLink.substring(startIndex, endIndex);
+                            } else {
+                                oAssetLink += iAssetLink.substring(startIndex, endIndex);
+                            }
+                            
+                            slideProperties.put("asset", oAssetLink);
+                        }                        
+                    }
+                    else // Video/Image
+                    {                        
+                        slideProperties.put("asset", slide.getValueMap().get("asset", String.class));
+                    }
+
                     slideProperties.put("richTextEditor", slide.getValueMap().get("richTextEditor", String.class));
                     slideProperties.put("assetType", slide.getValueMap().get("assetType", String.class));
-                    slideProperties.put("asset", slide.getValueMap().get("aseet", String.class));
+
                     slides.add(slideProperties);
                 }
             }
